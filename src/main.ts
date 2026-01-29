d924a41cdc2073542ac7629b61a19400735634e5
jeffzzy98 <jeffzzy98@gmail.com>
2026-01-29 08:38:28 +0000

    update

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,32 @@ import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api')
-  app.useGlobalInterceptors(new TransformInterceptor())
-  app.useGlobalFilters(new HttpExceptionFilter())
+
+  // 全局前缀
+  app.setGlobalPrefix('api');
+
+  // 全局拦截器 / 过滤器
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  // ⭐ Swagger 配置
+  const config = new DocumentBuilder()
+    .setTitle('AESC Backend API')
+    .setDescription('接口文档（SQL Template / Vector / DataModel）')
+    .setVersion('1.0')
+    // .addBearerAuth() // 如果你有 JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+  // 访问地址：http://localhost:3000/docs
+  // 实际接口地址前缀：http://localhost:3000/api/xxx
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
