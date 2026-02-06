bc3c91439d0fc2f4d87da373cc1badc9dc9ac260
jeffzzy98 <jeffzzy98@gmail.com>
2026-02-06 06:28:34 +0000

    feat: add public function

--- src/modules/holidays/dto/holidays.controller.ts
+++ src/modules/holidays/dto/holidays.controller.ts
@@ -5,30 +5,34 @@ import {Public} from '../../../common/decorators/public.decorator'
 // import { DaysPredictionQueryDto } from './dto/days-prediction-query.dto';
 // import {HoursPredictionQueryDto} from './dto/hours-prediction-query.dto'
 
-@Public()
+
 @Controller('holidays')
 export class HolidaysController {
     constructor(private readonly holidaysService: HolidaysService){}
 
     // 本年度剩余public holiday天数
+    @Public()
     @Get('remaining-holiday')
     getRemainingHoliday() {
         return this.holidaysService.getRemainingHoliday()
     }
 
     // 距离最近public holiday距离
+    @Public()
     @Get('latest-holiday')
     getLatestHoliday() {
         return this.holidaysService.getLatestHoliday()
     }
 
     // 距离周末天数
+    @Public()
     @Get('latest-weekend')
     getLastestWeekend() {
         return this.holidaysService.getLatestWeekend()
     }
 
     // 距离发薪日天数
+    @Public()
     @ApiQuery({
         name:"day",
         required:false,
