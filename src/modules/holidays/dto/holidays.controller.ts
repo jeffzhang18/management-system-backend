826dbb084d2bf91a1d144a839d0f59b1ae1b8ad5
jeffzzy98 <jeffzzy98@gmail.com>
2026-02-06 06:27:16 +0000

    feat: add public function

--- src/modules/holidays/dto/holidays.controller.ts
+++ src/modules/holidays/dto/holidays.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Put, Query } from '@nestjs/common';
 import { HolidaysService } from './holidays.service';
 import { ApiQuery } from '@nestjs/swagger';
+import {Public} from '../../../common/decorators/public.decorator'
 // import { DaysPredictionQueryDto } from './dto/days-prediction-query.dto';
 // import {HoursPredictionQueryDto} from './dto/hours-prediction-query.dto'
 
+@Public()
 @Controller('holidays')
 export class HolidaysController {
     constructor(private readonly holidaysService: HolidaysService){}
