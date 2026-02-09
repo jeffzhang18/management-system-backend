bbd056a3cfee9156936905e970def7aad30e859f
jeffzzy98 <jeffzzy98@gmail.com>
2026-02-09 07:37:45 +0000

    feat: add public function

--- src/modules/weather/weather.controller.ts
+++ src/modules/weather/weather.controller.ts
@@ -2,24 +2,27 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { WeatherService } from './weather.service';
 import { DaysPredictionQueryDto } from './dto/days-prediction-query.dto';
 import {HoursPredictionQueryDto} from './dto/hours-prediction-query.dto'
+import {Public} from '../../common/decorators/public.decorator'
 
 
 @Controller('weather')
 export class WeatherController {
   constructor(private readonly weatherService: WeatherService) {}
 
-  
+  @Public()
   @Get('now')
   getNow(@Query('location') location: string) {
     return this.weatherService.getNow(location);
   }
 
+  @Public()
   @Get('location')
   getLocation(@Query('location') location: string) {
     return this.weatherService.getLocation(location);
   }
 
   // http://47.117.245.39:3000/api/weather/days-prediction?location=101020100&days=30d
+  @Public()
   @Get('days-prediction')
   getDaysPrediction(
     @Query() query: DaysPredictionQueryDto,
@@ -28,6 +31,7 @@ export class WeatherController {
       query.days)
   }
 
+  @Public()
   @Get('hours-prediction')
   getHoursPrediction(@Query() query: HoursPredictionQueryDto) {
     return this.weatherService.getHoursPrediction(query.location, query.hours)
