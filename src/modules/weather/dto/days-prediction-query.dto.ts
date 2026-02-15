43978a5b0824d4daae29d5e0f76435df5c849a51
jeffzzy98 <jeffzzy98@gmail.com>
2026-02-15 06:06:48 +0000

    fix: fix the weather api dto

--- src/modules/weather/dto/days-prediction-query.dto.ts
+++ src/modules/weather/dto/days-prediction-query.dto.ts
@@ -1,5 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+
 export class DaysPredictionQueryDto {
+  @ApiProperty({
+    example: "101190202",
+    description:"地区ID"
+  })
+    @IsString()
     location: string;
+
+
+    @ApiProperty({
+      example: "3d",
+      description:"预测天数('3d' | '7d' | '10d' | '15d' | '30d')"
+    })
+    @IsString()
     days: '3d' | '7d' | '10d' | '15d' | '30d';
   }
   
--- src/modules/weather/dto/hours-prediction-query.dto.ts
+++ src/modules/weather/dto/hours-prediction-query.dto.ts
@@ -1,5 +1,22 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+
+
 export class HoursPredictionQueryDto {
-    hours: '24h' | '72h' | '168h';
+    @ApiProperty({
+      example: "101190202",
+      description:"地区ID"
+    })
+    @IsString()
     location: string;
+
+
+    @ApiProperty({
+      example: "24h",
+      description:"预测小时数('24h' | '72h' | '168h')"
+    })
+    @IsString()
+    hours: '24h' | '72h' | '168h';
+    
   }
   
