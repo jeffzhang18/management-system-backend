720543890c156fcafbb5f63e23331f1683e19d46
jeffzzy98 <jeffzzy98@gmail.com>
2026-03-05 02:04:02 +0000

    fix: fix known bugs

--- src/modules/notifier/notifier.module.ts
+++ src/modules/notifier/notifier.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { NotifierService } from './notifier.service';
 import { HolidaysModule } from '../holidays/holidays.module';
 import { WeatherModule } from '../weather/weather.module';
+import { NotifierController } from '../notifier/notifier.controller'
 
 
 @Module({
     imports: [HolidaysModule, WeatherModule], 
+    controllers: [NotifierController],
     providers: [NotifierService],
     exports: [NotifierService],
 })
