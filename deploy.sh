4fc92722d5b24884e9e6913b4fc67dafa925a28b
jeffzzy98 <jeffzzy98@gmail.com>
2026-03-05 02:35:56 +0000

    fix: fixed the deployment process

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,19 @@ echo "=============================="
 cd /var/www/management-system/management-system-backend
 
 echo "Pull latest code"
-git pull origin main
+git fetch --all
+git reset --hard origin/main
 
 echo "Install dependencies"
 pnpm install
 
+echo "Build project"
+pnpm build
+
 echo "Restart service with pm2"
 pm2 restart management-backend || \
-pm2 start pnpm --name management-backend -- run start:dev
+pm2 start dist/src/main.js --name management-backend --time
+
+pm2 save
 
 echo "Deployment finished"
