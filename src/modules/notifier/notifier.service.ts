2fc9c6aed5e4d66d811e531a37b6628cb2afc139
jeffzzy98 <jeffzzy98@gmail.com>
2026-05-12 06:42:34 +0000

    feat: update token cost notifier

--- src/modules/notifier/notifier.service.ts
+++ src/modules/notifier/notifier.service.ts
@@ -189,7 +189,7 @@ export class NotifierService {
     return text;
   }
 
-  @Cron('0 35 14 * * *', {
+  @Cron('0 05 9 * * *', {
     timeZone: 'Asia/Shanghai',
   })
   async handleDailyReminderForAnotherGroup() {
