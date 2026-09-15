287d5ef257c4ff5cab7291b64137f6b399235877
jeffzhang18 <jeffzzy98@gmail.com>
2026-09-15 03:02:48 +0000

    feat: delete the notifier api

--- src/modules/notifier/notifier.controller.ts
+++ src/modules/notifier/notifier.controller.ts
@@ -1,17 +1,4 @@
-import { Controller, Get, ParseIntPipe, Put, Query } from '@nestjs/common';
-import { NotifierService } from './notifier.service';
-import { ApiQuery } from '@nestjs/swagger';
-import {Public} from '../../common/decorators/public.decorator'
+import { Controller } from '@nestjs/common';
 
 @Controller('notifier')
-export class NotifierController {
-
-    constructor(private readonly notifierService: NotifierService){}
-
-    @Public()
-    @Get('wechat')
-    getRemainingHoliday() {
-        return this.notifierService.handleDailyReminder()
-    }
-
-}
+export class NotifierController {}
